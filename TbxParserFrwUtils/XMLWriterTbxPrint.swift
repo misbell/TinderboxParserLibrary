dbb297e8ce015ee832852bf0ba2c5b7b13d19710
michael isbell <prenezisbell@gmail.com>
2019-08-06 20:14:16 -0400

    first print of xml elements now working

---
+++ TbxParserFrwUtils/XMLWriterTbxPrint.swift
@@ -0,0 +1,9 @@
+//
+//  XMLWriterTbxPrint.swift
+//  TbxParserFrwUtils
+//
+//  Created by michael isbell on 8/6/19.
+//  Copyright © 2019 michael isbell. All rights reserved.
+//
+
+import Foundation
